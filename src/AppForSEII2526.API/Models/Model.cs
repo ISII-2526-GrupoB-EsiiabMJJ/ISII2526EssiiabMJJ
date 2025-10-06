ffa7a9551e25256a2fd95318ca374986730d6ca6
petruvl4d <petruvlad.pasat@alu.uclm.es>
2025-10-06 18:19:50 +0200

    Subida de la clase Model a la rama remota.#7 (#25)
    Co-authored-by: rumanoLoko <pasatpetruvlad@gmail.com>
    
    omprobamos que no hay conflictos entre la rama development y la clase Model.

---
+++ src/AppForSEII2526.API/Models/Model.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AppForSEII2526.API.Models
+{
+    public class Model
+    {
+
+        public Model(){}
+        public Model(int id, string nameModel)
+        {
+            this.Id = id;
+            this.NameModel = nameModel;
+        }
+
+        public bool Equals(object? obj)
+        {
+            if (obj is Model model)
+            {
+                return this.Id.Equals(model.Id);
+            }
+            return false;
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(Id, NameModel);
+        }
+
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "El nombre del modelo debe tener mínimo 2 caracteres")]
+        public string NameModel { get; set; }
+
+    }
+}
