7a455818bdcfccbb84ece287f2df646c515d7cae
Jaime Cátedra Rivas <jaime.catedra@alu.uclm.es>
2025-10-08 19:33:04 +0200

    Creación de ApplicationDbContext, Migración CreateIdentitySchema y modificación de clases. #52 (#53)
    Aceptar Creación de ApplicationDbContext, Migración CreateIdentitySchema y modificación de clases. #52

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.API.Models;
 
 namespace AppForSEII2526.API.Data
@@ -7,9 +6,7 @@ namespace AppForSEII2526.API.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
         public DbSet<Model> Model { get; set; }
         public DbSet<Device> Device { get; set; }
@@ -23,27 +20,22 @@ namespace AppForSEII2526.API.Data
         public DbSet<Receipt> Receipts { get; set; }
         public DbSet<ReceiptItem> ReceiptItems { get; set; }
 
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<PurchaseItem> PurchaseItems { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-
-            builder.Entity<ReviewItem>().HasKey(x => new { x.Id });
-            builder.Entity<Review>().HasKey(x => new { x.ReviewId });
-            builder.Entity<Model>().HasKey(x => new { x.Id });
-            builder.Entity<Device>().HasKey(x => new { x.Id });
-            builder.Entity<RentDevice>().HasKey(x => new { x.DeviceId });
-            builder.Entity<Rental>().HasKey(x => new { x.Id });
-
+            builder.Entity<ReviewItem>().HasKey(x => x.Id);
+            builder.Entity<Review>().HasKey(x => x.ReviewId);
+            builder.Entity<Model>().HasKey(x => x.Id);
+            builder.Entity<Device>().HasKey(x => x.Id);
+            builder.Entity<RentDevice>().HasKey(x => x.DeviceId);
+            builder.Entity<Rental>().HasKey(x => x.Id);
             builder.Entity<Scale>().HasKey(x => x.Id);
             builder.Entity<Repair>().HasKey(x => x.Id);
             builder.Entity<Receipt>().HasKey(x => x.Id);
             builder.Entity<ReceiptItem>().HasKey(x => x.Id);
-
-            builder.Entity<Repair>()
-                .HasOne(r => r.Scale)
-                .WithMany(s => s.Repairs)
-                .HasForeignKey(r => r.ScaleId)
-                .OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<ReceiptItem>()
                 .HasOne(ri => ri.Repair)
@@ -56,6 +48,93 @@ namespace AppForSEII2526.API.Data
                 .WithMany(r => r.ReceiptItems)
                 .HasForeignKey(ri => ri.ReceiptId)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Repair>()
+                .HasOne(r => r.Scale)
+                .WithMany(s => s.Repairs)
+                .HasForeignKey(r => r.ScaleId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.Entity<Receipt>()
+                .HasOne(r => r.ApplicationUser)
+                .WithMany(u => u.Receipts)     
+                .HasForeignKey(r => r.ApplicationUserId) 
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Purchase>()
+                .HasOne(p => p.ApplicationUser)
+                .WithMany(u => u.Purchases)
+                .HasForeignKey(p => p.ApplicationUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<PurchaseItem>()
+                .HasKey(pi => new { pi.DeviceId, pi.PurchaseId });
+
+            builder.Entity<PurchaseItem>()
+                .HasOne(pi => pi.Purchase)
+                .WithMany(p => p.Items)
+                .HasForeignKey(pi => pi.PurchaseId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<PurchaseItem>()
+                .HasOne(pi => pi.Device)
+                .WithMany() 
+                .HasForeignKey(pi => pi.DeviceId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ReviewItem>()
+                .HasOne(ri => ri.Device)
+                .WithMany(d => d.ReviewItems)
+                .HasForeignKey(ri => ri.DeviceId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<ReviewItem>()
+                .HasOne(ri => ri.Review)
+                .WithMany(r => r.ReviewItems)
+                .HasForeignKey(ri => ri.ReviewId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Review>()
+                .HasOne(r => r.ApplicationUser)
+                .WithMany(u => u.Reviews)
+                .HasForeignKey(r => r.ApplicationUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            
+            builder.Entity<RentDevice>()
+                .HasOne(ri => ri.Device)
+                .WithMany(d => d.RentedDevices)
+                .HasForeignKey(ri => ri.DeviceId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<RentDevice>()
+                .HasOne(ri => ri.Device)
+                .WithMany(r => r.RentedDevices)
+                .HasForeignKey(ri => ri.DeviceId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Rental>()
+                .HasOne(r => r.ApplicationUser)
+                .WithMany(u => u.Rentals)
+                .HasForeignKey(r => r.ApplicationUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Device>()
+                .HasOne(d => d.Model)
+                .WithMany(m => m.Devices)
+                .HasForeignKey(d => d.ModelId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
