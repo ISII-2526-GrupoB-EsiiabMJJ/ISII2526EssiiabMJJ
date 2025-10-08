86ad7551bd4aa9e93273c6d62c1350221ca8d51e
JAVIER SÁNCHEZ VICENTE <Javier.Sanchez73@alu.uclm.es>
2025-10-08 14:07:21 +0200

    Commit Modificación de clases y diagrama de clases para añadir ApplicationUser #46

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -1,7 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
-namespace AppForSEII2526.API.Models;
+namespace AppForSEII2526.API.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() { }
 
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser {
+        public ApplicationUser(string id, string name, string surname, string userName)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            UserName = userName;
+            Email = userName;
+        }
+
+        [Display(Name = "Name")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Surname")]
+        public string? Surname { get; set; }
+
+        public ICollection<Receipt> Receipt { get; set; } = new List<Receipt>();
+    }
 }
