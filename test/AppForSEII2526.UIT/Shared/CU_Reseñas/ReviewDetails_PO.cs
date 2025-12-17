4ec965a74c95466c901d894d971d02861aa2c146
rumanoLoko <pasatpetruvlad@gmail.com>
2025-12-17 18:22:51 +0100

    Subida de componentes Page Object aparte del conjunto de todas las pruebas funcionales aparte de ajuste del fichero Program.cs

---
+++ test/AppForSEII2526.UIT/Shared/CU_Reseñas/ReviewDetails_PO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForMovies.UIT.CU_Reseñas
+{
+    public class ReviewDetails_PO : PageObject
+    {
+        public ReviewDetails_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public bool CheckReviewDetail(string? customerName, string customerCountry, string reviewTitle,
+            List<string[]> expectedReviewed, By tableIdBy)
+        {
+            WaitForBeingVisible(By.Id("ReviwedDevices"));
+            bool result = true;
+            result = result && _driver.FindElement(By.Id("CustomerName")).Text.Contains(customerName);
+            result = result && _driver.FindElement(By.Id("CustomerCountry")).Text.Contains(customerCountry);
+            result = result && _driver.FindElement(By.Id("ReviewTitle")).Text.Contains(reviewTitle);
+
+            result = result && CheckBodyTable(expectedReviewed, tableIdBy);
+            return result;
+
+        }
+
+        public bool CheckListOfMovies(List<string[]> expectedRentalItems)
+        {
+            return CheckBodyTable(expectedRentalItems, By.Id("RentedMovies"));
+        }
+    }
+}
