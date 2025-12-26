fc59e1b4b7ff9e7ebdac5e9d75e45e38c3cd9fe3
Jaime Cátedra Rivas <jaime.catedra@alu.uclm.es>
2025-12-26 13:31:11 +0100

    112 desarrollar UI para crear un alquiler (#114)
    * Desarrollo del StateContainer relacionada con tel caso de uso de Alquilar un dispositivo #112
    
    * Incorporacion de RentalStateContainer en el archivo Program.cs #112
    
    * Creados los Razors de ListDeviceForRenting y RentalItem. #113

---
+++ src/AppForSEII2526.Web/RentalStateContainer.cs
@@ -0,0 +1,70 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class RentalStateContainer{
+        //Hemos creado una instancia de Alquiler para cuando se cree una instancia de RentalStateContainer
+        public RentalForCreateDTO Rental { get; private set; } = new RentalForCreateDTO()
+        {
+            RentalItems = new List<RentalItemDTO>()
+        };
+
+        // Hemos computado el precio total (TotalPrice) de los dispositivos seleccionados para alquilarlos
+        public decimal TotalPrice
+        {
+            get
+            {
+                int numberOfDays = (Rental.RentalDateTo - Rental.RentalDateFrom).Days;
+                return Convert.ToDecimal(Rental.RentalItems.Sum(ri => ri.PriceForRent * numberOfDays));
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+
+        public void AddDeviceToRental(RentalForCreateDTO rental)
+        {
+            foreach (var rentalItem in rental.RentalItems)
+            {
+                if (!Rental.RentalItems.Any(ri => ri.DeviceID == rentalItem.DeviceID))
+                {
+                    // Añadimos el dispositivo si no está en la lista
+                    Rental.RentalItems.Add(new RentalItemDTO()
+                    {
+                        DeviceID = rentalItem.DeviceID,
+                        PriceForRent = rentalItem.PriceForRent,
+                        Description = rentalItem.Description,
+                        Model = rentalItem.Model
+                    });
+                }
+            }
+
+        }
+        //para borrar dispositivos de la lista de dispositivos seleccionados
+        public void RemoveRentalItemToRent(RentalItemDTO item)
+        {
+            Rental.RentalItems.Remove(item);
+
+        }
+        //Hemos eliminado todos los dispositivos de la lista
+        public void ClearRentingCart()
+        {
+            Rental.RentalItems.Clear();
+
+        }
+
+        
+        //Casi hemos terminado el proceso de alquiler, por lo que, creamos un nuevo Alquiler
+        public void RentalProcessed()
+        {
+            //Tenemos que terminar el proceso de alquilar, por lo que creamos un nuevo objeto sin datos
+            Rental = new RentalForCreateDTO()
+            {
+                RentalItems = new List<RentalItemDTO>()
+            };
+        }
+    }
+}
