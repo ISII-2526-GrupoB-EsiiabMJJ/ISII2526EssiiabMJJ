09622afe1bd2e86fd03bf2cd12d91ad84bb4e83c
mariatdm <Maria.Torres15@alu.uclm.es>
2026-04-29 16:31:10 +0200

    feat(api): add PurchaseDetailDTO

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseDetailDTO.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+{
+    public class PurchaseDetailDTO
+    {
+        public PurchaseDetailDTO(
+            int id,
+            string name,
+            string surname,
+            string deliveryAddress,
+            DateTime purchaseDateUtc,
+            double totalPrice,
+            int totalQuantity,
+            IList<PurchaseItemDTO> purchaseItems)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            DeliveryAddress = deliveryAddress;
+            PurchaseDateUtc = purchaseDateUtc;
+            TotalPrice = totalPrice;
+            TotalQuantity = totalQuantity;
+            PurchaseItems = purchaseItems;
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string Surname { get; set; }
+
+        [StringLength(200)]
+        public string DeliveryAddress { get; set; }
+
+        public DateTime PurchaseDateUtc { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        public int TotalQuantity { get; set; }
+
+        public IList<PurchaseItemDTO> PurchaseItems { get; set; }
+    }
+}
