e4287cb8811aedb31711f614a170c102bf401ef3
JAVIER SÁNCHEZ VICENTE <Javier.Sanchez73@alu.uclm.es>
2026-05-11 09:22:43 +0200

    Añadidos los DTOs #68

---
+++ src/AppForSEII2526.API/DTOs/RepairDTOs/ReceiptDetailDTO.cs
@@ -0,0 +1,48 @@
+﻿using AppForSEII2526.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.DTOs.RepairDTOs
+{
+    public class ReceiptDetailDTO
+    {
+        public ReceiptDetailDTO()
+        {
+            ReceiptItems = new List<ReceiptItemDTO>();
+        }
+
+        public ReceiptDetailDTO(
+            int id,
+            string customerNameSurname,
+            string deliveryAddress,
+            PaymentMethodTypes paymentMethod,
+            DateTime receiptDate,
+            decimal totalPrice,
+            IList<ReceiptItemDTO> receiptItems)
+        {
+            Id = id;
+            CustomerNameSurname = customerNameSurname;
+            DeliveryAddress = deliveryAddress;
+            PaymentMethod = paymentMethod;
+            ReceiptDate = receiptDate;
+            TotalPrice = totalPrice;
+            ReceiptItems = receiptItems;
+        }
+
+        public int Id { get; set; }
+
+        [StringLength(150)]
+        public string CustomerNameSurname { get; set; } = string.Empty;
+
+        [StringLength(250)]
+        public string DeliveryAddress { get; set; } = string.Empty;
+
+        public PaymentMethodTypes PaymentMethod { get; set; }
+
+        public DateTime ReceiptDate { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        public decimal TotalPrice { get; set; }
+
+        public IList<ReceiptItemDTO> ReceiptItems { get; set; }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/RepairDTOs/ReceiptForCreateDTO.cs
@@ -0,0 +1,44 @@
+﻿using AppForSEII2526.API.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.DTOs.RepairDTOs
+{
+    public class ReceiptForCreateDTO
+    {
+        public ReceiptForCreateDTO()
+        {
+            ReceiptItems = new List<ReceiptItemForCreateDTO>();
+        }
+
+        public ReceiptForCreateDTO(
+            string customerUserName,
+            string customerNameSurname,
+            string deliveryAddress,
+            PaymentMethodTypes paymentMethod,
+            IList<ReceiptItemForCreateDTO> receiptItems)
+        {
+            CustomerUserName = customerUserName;
+            CustomerNameSurname = customerNameSurname;
+            DeliveryAddress = deliveryAddress;
+            PaymentMethod = paymentMethod;
+            ReceiptItems = receiptItems;
+        }
+
+        [Required]
+        public string CustomerUserName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(150, MinimumLength = 3)]
+        public string CustomerNameSurname { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(250, MinimumLength = 5)]
+        public string DeliveryAddress { get; set; } = string.Empty;
+
+        [Required]
+        public PaymentMethodTypes PaymentMethod { get; set; }
+
+        [Required]
+        public IList<ReceiptItemForCreateDTO> ReceiptItems { get; set; }
+    }
+}
