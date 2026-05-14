fccd1c16f98b59b8554d6caf032769c8a7f80db8
Javier-SV <73502411+Javier-SV@users.noreply.github.com>
2026-05-14 08:58:29 +0200

    Añadidos los DTOs #68 (#165)
    Se cierra #68

---
+++ src/AppForSEII2526.API/DTOs/RepairDTOs/ReceiptItemDTO.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.DTOs.RepairDTOs
+{
+    public class ReceiptItemDTO
+    {
+        public ReceiptItemDTO()
+        {
+        }
+
+        public ReceiptItemDTO(
+            string model,
+            int repairId,
+            string repairName,
+            string repairDescription,
+            decimal repairCost,
+            string scale)
+        {
+            Model = model;
+            RepairId = repairId;
+            RepairName = repairName;
+            RepairDescription = repairDescription;
+            RepairCost = repairCost;
+            Scale = scale;
+        }
+
+        [StringLength(100)]
+        public string Model { get; set; } = string.Empty;
+
+        public int RepairId { get; set; }
+
+        [StringLength(100)]
+        public string RepairName { get; set; } = string.Empty;
+
+        [StringLength(300)]
+        public string RepairDescription { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue)]
+        public decimal RepairCost { get; set; }
+
+        [StringLength(50)]
+        public string Scale { get; set; } = string.Empty;
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/RepairDTOs/ReceiptItemForCreateDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppForSEII2526.API.DTOs.RepairDTOs
+{
+    public class ReceiptItemForCreateDTO
+    {
+        public ReceiptItemForCreateDTO()
+        {
+        }
+
+        public ReceiptItemForCreateDTO(string model, int repairId)
+        {
+            Model = model;
+            RepairId = repairId;
+        }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
+        public string Model { get; set; } = string.Empty;
+
+        [Required]
+        public int RepairId { get; set; }
+    }
+}
