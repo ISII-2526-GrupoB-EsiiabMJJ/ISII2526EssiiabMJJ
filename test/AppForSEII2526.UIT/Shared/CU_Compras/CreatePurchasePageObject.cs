3e6c1087188bb8292c0849583ff531d1921009a6
mariatdm <Maria.Torres15@alu.uclm.es>
2026-05-14 12:57:18 +0200

    test(uit): añadir page objects para compra

---
+++ test/AppForSEII2526.UIT/Shared/CU_Compras/CreatePurchasePageObject.cs
@@ -0,0 +1,91 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Xunit.Abstractions;
+
+namespace AppForSEII2526.UIT.Shared.CU_Compras;
+
+public class CreatePurchasePageObject : PageObject
+{
+    private readonly By _purchaseName = By.Id("purchaseName");
+    private readonly By _purchaseSurname = By.Id("purchaseSurname");
+    private readonly By _purchaseDeliveryAddress = By.Id("purchaseDeliveryAddress");
+    private readonly By _paymentMethodSelected = By.Id("paymentMethodSelected");
+    private readonly By _modifyPurchaseCart = By.Id("modifyPurchaseCart");
+    private readonly By _savePurchase = By.Id("savePurchase");
+    private readonly By _createPurchaseTotalQuantity = By.Id("createPurchaseTotalQuantity");
+    private readonly By _createPurchaseTotalPrice = By.Id("createPurchaseTotalPrice");
+    private readonly By _purchaseNameError = By.Id("purchaseNameError");
+    private readonly By _purchaseSurnameError = By.Id("purchaseSurnameError");
+    private readonly By _purchaseDeliveryAddressError = By.Id("purchaseDeliveryAddressError");
+    private readonly By _createPurchaseError = By.Id("createPurchaseError");
+
+    public CreatePurchasePageObject(IWebDriver driver, ITestOutputHelper output)
+        : base(driver, output)
+    {
+    }
+
+    public void FillCustomerData(string name, string surname, string deliveryAddress)
+    {
+        WaitForBeingVisible(_purchaseName);
+
+        _driver.FindElement(_purchaseName).Clear();
+        _driver.FindElement(_purchaseName).SendKeys(name);
+
+        _driver.FindElement(_purchaseSurname).Clear();
+        _driver.FindElement(_purchaseSurname).SendKeys(surname);
+
+        _driver.FindElement(_purchaseDeliveryAddress).Clear();
+        _driver.FindElement(_purchaseDeliveryAddress).SendKeys(deliveryAddress);
+    }
+
+    public void SelectPaymentMethod(string paymentMethodValue)
+    {
+        WaitForBeingVisible(_paymentMethodSelected);
+        var select = new SelectElement(_driver.FindElement(_paymentMethodSelected));
+        select.SelectByValue(paymentMethodValue);
+    }
+
+    public void SavePurchase()
+    {
+        WaitForBeingClickable(_savePurchase);
+        _driver.FindElement(_savePurchase).Click();
+    }
+
+    public void ModifyCart()
+    {
+        WaitForBeingClickable(_modifyPurchaseCart);
+        _driver.FindElement(_modifyPurchaseCart).Click();
+    }
+
+    public string GetTotalQuantity()
+    {
+        WaitForBeingVisible(_createPurchaseTotalQuantity);
+        return _driver.FindElement(_createPurchaseTotalQuantity).Text;
+    }
+
+    public string GetTotalPrice()
+    {
+        WaitForBeingVisible(_createPurchaseTotalPrice);
+        return _driver.FindElement(_createPurchaseTotalPrice).Text;
+    }
+
+    public bool HasNameError()
+    {
+        return _driver.FindElements(_purchaseNameError).Any();
+    }
+
+    public bool HasSurnameError()
+    {
+        return _driver.FindElements(_purchaseSurnameError).Any();
+    }
+
+    public bool HasDeliveryAddressError()
+    {
+        return _driver.FindElements(_purchaseDeliveryAddressError).Any();
+    }
+
+    public bool HasCreatePurchaseError()
+    {
+        return _driver.FindElements(_createPurchaseError).Any();
+    }
+}
---
+++ test/AppForSEII2526.UIT/Shared/CU_Compras/DetailPurchasePageObject.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium;
+using Xunit.Abstractions;
+
+namespace AppForSEII2526.UIT.Shared.CU_Compras;
+
+public class DetailPurchasePageObject : PageObject
+{
+    private readonly By _purchaseDetail = By.Id("purchaseDetail");
+    private readonly By _purchaseCustomerName = By.Id("purchaseCustomerName");
+    private readonly By _purchaseCustomerSurname = By.Id("purchaseCustomerSurname");
+    private readonly By _purchaseDeliveryAddress = By.Id("purchaseDeliveryAddress");
+    private readonly By _purchaseDate = By.Id("purchaseDate");
+    private readonly By _purchaseTotalQuantity = By.Id("purchaseTotalQuantity");
+    private readonly By _purchaseTotalPrice = By.Id("purchaseTotalPrice");
+    private readonly By _purchaseDetailItemsTable = By.Id("PurchaseDetailItemsTable");
+    private readonly By _newPurchaseButton = By.Id("newPurchaseButton");
+
+    public DetailPurchasePageObject(IWebDriver driver, ITestOutputHelper output)
+        : base(driver, output)
+    {
+    }
+
+    public bool IsLoaded()
+    {
+        return _driver.FindElements(_purchaseDetail).Any();
+    }
+
+    public string GetCustomerName()
+    {
+        WaitForBeingVisible(_purchaseCustomerName);
+        return _driver.FindElement(_purchaseCustomerName).Text;
+    }
+
+    public string GetCustomerSurname()
+    {
+        WaitForBeingVisible(_purchaseCustomerSurname);
+        return _driver.FindElement(_purchaseCustomerSurname).Text;
+    }
+
+    public string GetDeliveryAddress()
+    {
+        WaitForBeingVisible(_purchaseDeliveryAddress);
+        return _driver.FindElement(_purchaseDeliveryAddress).Text;
+    }
+
+    public string GetTotalQuantity()
+    {
+        WaitForBeingVisible(_purchaseTotalQuantity);
+        return _driver.FindElement(_purchaseTotalQuantity).Text;
+    }
+
+    public string GetTotalPrice()
+    {
+        WaitForBeingVisible(_purchaseTotalPrice);
+        return _driver.FindElement(_purchaseTotalPrice).Text;
+    }
+
+    public bool IsItemsTableVisible()
+    {
+        return _driver.FindElements(_purchaseDetailItemsTable).Any();
+    }
+
+    public void StartNewPurchase()
+    {
+        WaitForBeingClickable(_newPurchaseButton);
+        _driver.FindElement(_newPurchaseButton).Click();
+    }
+}
